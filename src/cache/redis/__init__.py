47aa1c83a256881372c338585e0644b8473bcd9d
BrenDev0 <lahey1991@gmail.com>
2026-06-16 10:05:10 -0600

    redis cache store

--- src/cache/redis/__init__.py
+++ src/cache/redis/__init__.py
@@ -1,14 +1,47 @@
-from redis import asyncio as redis
+import redis.asyncio as redis
 
+import json
 from typing import Any
+
 
 class RedisCacheStore:
     def __init__(self, connection_url: str):
-        self.redis = redis.from_url(connection_url)
+        self._redis = redis.from_url(connection_url)
 
     async def store(
         self,
         key: str,
-        data: dict[str, Any]
-    ):
-        pass
+        data: dict[str, Any],
+        expire_seconds: int
+    )-> bool:
+        return await self._redis.set(
+            name=key,
+            value=json.dumps(data),
+            ex=expire_seconds
+        )
+    
+    async def get(self, key: str) -> dict[str, Any] | None:
+        data = await self._redis.get(name=key)
+        if data is None:
+            return None
+        
+        return json.loads(data)
+    
+    async def expire(
+        self, 
+        key:str, 
+        expipre_seconds: int
+    )-> bool: 
+        return await self._redis.expire(
+            name=key,
+            time=expipre_seconds
+        )
+    
+    async def increment(self, key: str) -> int:
+        return await self._redis.incr(name=key)
+    
+    async def remove(self, key: str) -> bool:
+        return await self._redis.delete(key) > 0
+    
+    async def close_connection(self):
+        await self._redis.close()
