61c1dfc29f5b8e293d8fe7eaa791f8ab50483b6f
BrenDev0 <lahey1991@gmail.com>
2026-06-30 09:38:09 -0600

    mappers to and from db

---
+++ src/assistants/sqlalchemy/mappers.py
@@ -0,0 +1,17 @@
+from dataclasses import asdict
+
+from ..models import Assistant, AssistantCreate
+from .models import AssistantRow
+
+
+def row_to_domain(row: AssistantRow) -> Assistant:
+    return Assistant(
+        id=row.id,
+        user_id=row.user_id,
+        name=row.name,
+        description=row.description
+    )
+
+
+def domain_create_to_row(domain_create: AssistantCreate) -> AssistantRow:
+    return AssistantRow(**asdict(domain_create))
